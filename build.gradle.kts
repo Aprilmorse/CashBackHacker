c75cbbf38a2a03ab46503cd838af586bcdefb7eb
79911 <Petrovlistov@gmail.com>
2024-03-18 20:37:42 +0300

    add test for test junit4-platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,27 @@
 plugins {
-    id("java")
+    id 'java'
 }
 
-group = "ru.netology"
-version = "1.0-SNAPSHOT"
+sourceCompatibility = 11
+compileJava.options.encoding = "UTF-8"
+compileTestJava.options.encoding = "UTF-8"
+
+group 'ru.netology'
+version '1.0-SNAPSHOT'
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation 'org.junit.jupiter:junit-jupiter-api:5.6.1'
+    testImplementation 'org.junit.jupiter:junit-jupiter:5.8.1'
+    testRuntimeOnly 'org.junit.jupiter:junit-jupiter-engine:5.6.1'
+
+    testImplementation 'junit:junit:4.13'
+    testRuntimeOnly 'org.junit.vintage:junit-vintage-engine:5.6.2'
 }
 
-tasks.test {
+test {
     useJUnitPlatform()
 }
